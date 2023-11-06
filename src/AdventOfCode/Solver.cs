63b47fc1bcee26366b275bde1d2d9b8b9a72fb37
Nils Gehlin <nils.gehlin@gmail.com>
2023-11-06 16:28:42 +0100

    some refactoroing

--- src/AdventOfCode/Solver.cs
+++ src/AdventOfCode/Solver.cs
@@ -6,13 +6,61 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
-public record Solution(int Year, int Day, MethodInfo Part1, MethodInfo Part2);
+public record Solution(int Year, int Day, int Part, MethodInfo Method);
 
 public static class Solver
 {
     private const string DataDir = "../../data";
 
     public static void RunSolutions(int? year, int? day, int? part, bool runExample)
+    {
+        var solutions = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .FindSolutionsToRun(year, day, part);
+
+        foreach (var solution in solutions)
+        {
+            var extension = runExample ? "example" : "in";
+            var input = File.ReadAllText($"{DataDir}/{solution.Year}/Day{solution.Day}.{extension}");
+            var args = new string[] { input };
+            var result = (string?)solution.Method.Invoke(null, args);
+
+            Console.Write($"{solution.Year}, Day {solution.Day}, Part {solution.Part}: {result} ");
+
+            if (runExample)
+            {
+                Console.WriteLine();
+                continue;
+            }
+
+            var answerFilePath = $"{DataDir}/{solution.Year}/Day{solution.Day}.out";
+            if (!File.Exists(answerFilePath))
+            {
+                Console.WriteLine("??");
+                continue;
+            }
+
+            var answer = File.ReadAllText(answerFilePath);
+            if (answer == result)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("+");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("-");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    private static IEnumerable<Solution> FindSolutionsToRun(
+        this IEnumerable<Type> types,
+        int? year,
+        int? day,
+        int? part)
     {
         var assemblyTypes = Assembly.GetExecutingAssembly().GetTypes();
 
@@ -48,39 +96,8 @@ public static class Solver
                     continue;
                 }
 
-                var extension = runExample ? "example" : "in";
-                var input = File.ReadAllText($"{DataDir}/{solutionAttr.Year}/Day{solutionAttr.Day}.{extension}");
-                var args = new string[] { input };
-                var result = (string?)method.Invoke(null, args);
-
-                Console.Write($"{solutionAttr.Year}, Day {solutionAttr.Day}, Part {partAttribute.Part}: {result} ");
-
-                if (runExample)
-                {
-                    Console.WriteLine();
-                    continue;
-                }
-
-                var answerFilePath = $"{DataDir}/{solutionAttr.Year}/Day{solutionAttr.Day}.out";
-                if (!File.Exists(answerFilePath))
-                {
-                    Console.WriteLine("??");
-                    continue;
-                }
-
-                var answer = File.ReadAllText(answerFilePath);
-                if (answer == result)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("+");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("-");
-                    Console.ResetColor();
-                }
+                yield return new Solution(
+                    solutionAttr.Year, solutionAttr.Day, partAttribute.Part, method);
             }
         }
     }
