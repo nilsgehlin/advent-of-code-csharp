c37f57f93a2659b3bfb661258def7213366b6a08
Nils Gehlin <nils.gehlin@gmail.com>
2023-12-09 11:26:27 +0100

    starting day2

---
+++ src/AdventOfCode/2023/Day2.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Lib;
+
+namespace AdventOfCode.Y2023;
+
+[Solution(Year = 2023, Day = 2)]
+public class Day2
+{
+    [Part(1)]
+    public static int SolvePartOne(string input)
+    {
+        return 0;
+    }
+
+    [Part(2)]
+    public static int SolvePartTwo(string input)
+    {
+        return 0;
+    }
+}
