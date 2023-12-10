d5deb8a9f7995e5e46e68ca3ba286d714173e352
Nils Gehlin <nils.gehlin@gmail.com>
2023-12-10 14:25:40 +0100

    day 2 part 1 works

--- src/AdventOfCode/2023/Day2.cs
+++ src/AdventOfCode/2023/Day2.cs
@@ -1,19 +1,58 @@
-﻿using AdventOfCode.Lib;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode.Y2023;
 
 [Solution(Year = 2023, Day = 2)]
-public class Day2
+public partial class Day2
 {
-    [Part(1)]
-    public static int SolvePartOne(string input)
+    private static readonly Dictionary<string, uint> _colorDistribution = new()
+   {
+       {"red", 12},
+       {"green", 13},
+       {"blue", 14}
+   };
+
+    private static bool IsPossible(string line)
     {
-        return 0;
+        foreach (Match match in GetColorAndQtyRegex().Matches(line))
+        {
+            var qty = int.Parse(match.Value);
+            var color = match.Groups[1].Value;
+
+            if (qty > _colorDistribution[color])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
+
+    [GeneratedRegex(@"\d+(?= (red|green|blue))")]
+    private static partial Regex GetColorAndQtyRegex();
+
+    private static int GetId(string line) =>
+        int.Parse(GetGameIdRegEx().Match(line).Value);
+
+    [GeneratedRegex(@"(?<=Game )\d+(?=:)")]
+    private static partial Regex GetGameIdRegEx();
+
+    [Part(1)]
+    public static int SolvePartOne(string input) =>
+        input.Split(Environment.NewLine).ToArray()
+             .Where(line => line != string.Empty)
+             .Where(IsPossible)
+             .Select(GetId)
+             .Sum();
 
     [Part(2)]
     public static int SolvePartTwo(string input)
     {
         return 0;
     }
+
 }
