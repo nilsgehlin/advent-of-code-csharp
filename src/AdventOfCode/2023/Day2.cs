8c3d18b43d0ffc39ac240b3bc21d31ef436e2fe5
Nils Gehlin <nils.gehlin@gmail.com>
2023-12-10 17:32:01 +0100

    day 2 complete

--- src/AdventOfCode/2023/Day2.cs
+++ src/AdventOfCode/2023/Day2.cs
@@ -6,40 +6,18 @@ using AdventOfCode.Lib;
 
 namespace AdventOfCode.Y2023;
 
+public record CubeQty(string Color, int Qty);
+
+
 [Solution(Year = 2023, Day = 2)]
 public partial class Day2
 {
-    private static readonly Dictionary<string, uint> _colorDistribution = new()
-   {
-       {"red", 12},
-       {"green", 13},
-       {"blue", 14}
-   };
-
-    private static bool IsPossible(string line)
+    private static readonly Dictionary<string, int> _colorDistribution = new()
     {
-        foreach (Match match in GetColorAndQtyRegex().Matches(line))
-        {
-            var qty = int.Parse(match.Value);
-            var color = match.Groups[1].Value;
-
-            if (qty > _colorDistribution[color])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    [GeneratedRegex(@"\d+(?= (red|green|blue))")]
-    private static partial Regex GetColorAndQtyRegex();
-
-    private static int GetId(string line) =>
-        int.Parse(GetGameIdRegEx().Match(line).Value);
-
-    [GeneratedRegex(@"(?<=Game )\d+(?=:)")]
-    private static partial Regex GetGameIdRegEx();
+        {"red", 12},
+        {"green", 13},
+        {"blue", 14}
+    };
 
     [Part(1)]
     public static int SolvePartOne(string input) =>
@@ -50,9 +28,62 @@ public partial class Day2
              .Sum();
 
     [Part(2)]
-    public static int SolvePartTwo(string input)
+    public static int SolvePartTwo(string input) =>
+        input.Split(Environment.NewLine).ToArray()
+             .Where(line => line != string.Empty)
+             .Select(PowerOfMinimumSets)
+             .Sum();
+
+    private static IEnumerable<CubeQty> ParseCudeQty(string gameLine)
     {
-        return 0;
+        foreach (Match match in GetColorAndQtyRegex().Matches(gameLine))
+        {
+            var qty = int.Parse(match.Value);
+            var color = match.Groups[1].Value;
+
+            yield return new CubeQty(color, qty);
+        }
     }
 
+    private static bool IsPossible(string line)
+    {
+        foreach (var cubeQty in ParseCudeQty(line))
+        {
+            if (cubeQty.Qty > _colorDistribution[cubeQty.Color])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static int GetId(string line) =>
+        int.Parse(GetGameIdRegEx().Match(line).Value);
+
+    public static int PowerOfMinimumSets(string line)
+    {
+        var maxQty = new Dictionary<string, int>()
+        {
+            {"red", 0},
+            {"green", 0},
+            {"blue", 0}
+        };
+
+        foreach (var cubeQty in ParseCudeQty(line))
+        {
+            if (cubeQty.Qty > maxQty[cubeQty.Color])
+            {
+                maxQty[cubeQty.Color] = cubeQty.Qty;
+            }
+        }
+
+        return maxQty.Values.Aggregate(1, (accum, maxVal) => accum * maxVal);
+    }
+
+    [GeneratedRegex(@"\d+(?= (red|green|blue))")]
+    private static partial Regex GetColorAndQtyRegex();
+
+    [GeneratedRegex(@"(?<=Game )\d+(?=:)")]
+    private static partial Regex GetGameIdRegEx();
 }
