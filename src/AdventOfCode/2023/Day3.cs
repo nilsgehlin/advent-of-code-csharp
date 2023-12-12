7228a76840ff83053865733b30f9073c60c2b1c7
Nils Gehlin <nils.gehlin@gmail.com>
2023-12-12 14:17:34 +0100

    Started day3

--- src/AdventOfCode/2023/Day3.cs
+++ src/AdventOfCode/2023/Day3.cs
@@ -1,4 +1,7 @@
-﻿using AdventOfCode.Lib;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AdventOfCode.Lib;
 
 namespace AdventOfCode.Y2023;
 
@@ -8,11 +11,24 @@ public partial class Day3
 {
 
     [Part(1)]
-    public static int SolvePartOne(string input) =>
-        0;
+    public static int SolvePartOne(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        for (var lineIdx = 0; lineIdx < lines.Length; lineIdx++)
+        {
+            line = lines[lineIdx];
+            foreach (Match match in SymbolsRegex().Matches(line))
+            {
+
+            }
+        }
+    }
 
     [Part(2)]
     public static int SolvePartTwo(string input) =>
         0;
 
+    [GeneratedRegex(@"[^\d.\s]")]
+    private static partial Regex SymbolsRegex();
 }
+
