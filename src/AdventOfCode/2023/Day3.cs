d39d583e03792c8e39036930c453b67d982cbc6e
Nils Gehlin <nils.gehlin@gmail.com>
2023-12-15 12:48:46 +0100

    day3 part 1 done

--- src/AdventOfCode/2023/Day3.cs
+++ src/AdventOfCode/2023/Day3.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using AdventOfCode.Lib;
 
 namespace AdventOfCode.Y2023;
 
+public record Range(int Lower, int Upper);
+public record Number(int Value, int RowIdx, Range ColumnIdxRange);
 
 [Solution(Year = 2023, Day = 3)]
 public partial class Day3
@@ -13,47 +16,41 @@ public partial class Day3
     [Part(1)]
     public static int SolvePartOne(string input)
     {
-        var rows = input.Split(Environment.NewLine).Where(line => line != string.Empty).ToArray();
-        foreach (var row in rows)
+        var lines = input.Split(Environment.NewLine)
+            .Where(line => line != string.Empty);
+
+        return lines
+            .SelectMany((line, idx) => NumbersRegex().Matches(line).Select(match => (match, idx)))
+            .Select(matchAndIdx => new Number(int.Parse(matchAndIdx.match.Value),
+                                              matchAndIdx.idx,
+                                              new Range(matchAndIdx.match.Index, matchAndIdx.match.Index + matchAndIdx.match.Value.Length - 1)))
+            .Where(number => IsPartNumber(number, lines))
+            .Select(number => number.Value)
+            .Sum();
+    }
+
+    private static bool IsPartNumber(Number number, IEnumerable<string> lines)
+    {
+        for (var i = Math.Max(number.RowIdx - 1, 0); i <= Math.Min(number.RowIdx + 1, lines.Count() - 1); i++)
         {
-            Console.WriteLine("Matches:");
-            foreach (Match match in NumbersRegex().Matches(row))
+            for (var j = Math.Max(number.ColumnIdxRange.Lower - 1, 0); j <= Math.Min(number.ColumnIdxRange.Upper + 1, lines.ElementAt(i).Length - 1); j++)
             {
-                Console.WriteLine($"  {match.Index}");
+                if (SymbolsRegex().IsMatch(char.ToString(lines.ElementAt(i)[j])))
+                {
+                    return true;
+                }
             }
         }
 
-        return 0;
+        return false;
     }
-
-    // private static bool IsPartNumber(string[] rows, int rowIdx, int colIdx)
-    // {
-    //     if (!char.IsDigit(rows[rowIdx][colIdx])) return false;
-    //
-    //     char[] neighbours = [];
-    //
-    //     // Console.WriteLine($"{rowIdx}({rows.Length}),{colIdx}({rows[rowIdx].Length})");
-    //     for (var k = Math.Max(rowIdx - 1, 0); k <= Math.Min(rowIdx + 1, rows.Length - 1); k++)
-    //     {
-    //         for (var l = Math.Max(colIdx - 1, 0); l <= Math.Min(colIdx + 1, rows[rowIdx].Length - 1); l++)
-    //         {
-    //             if (k == rowIdx && l == colIdx) continue;
-    //
-    //             // Console.WriteLine($"  {k},{l}");
-    //             if (SymbolsRegex().IsMatch(char.ToString(rows[k][l])))
-    //             {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //
-    //     return false;
-    // }
-
 
     [Part(2)]
     public static int SolvePartTwo(string input) =>
         0;
+
+    [GeneratedRegex(@"[^\d.\s]")]
+    private static partial Regex SymbolsRegex();
 
     [GeneratedRegex(@"\d{1,}")]
     private static partial Regex NumbersRegex();
